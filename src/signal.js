a882e3feb659e30e98ae2bd239ac5770f589ce9f
Ashritha M <99526903+ashrithamanepalli@users.noreply.github.com>
2022-06-01 10:14:05 +0530

    Added two methods getNewState and updateState

--- src/signal.js
+++ src/signal.js
@@ -1,8 +1,18 @@
 class Signal {
   constructor() {
     this.state = 'red';
-    this.allStates = ['green', 'red'];
+    this.allStates = ['red', 'green'];
     this.index = 0;
+  }
+
+  getNextState() {
+    this.index++;
+    this.index = this.index >= this.allStates.length ? 0 : this.index;
+    return this.allStates[this.index];
+  }
+
+  updateState() {
+    this.state = this.getNextState();
   }
 
   equals(otherSignal) {
--- test/testSignal.js
+++ test/testSignal.js
@@ -8,4 +8,36 @@ describe('Signal', () => {
 
     assert.ok(signal1.equals(signal2));
   });
+
+  it('should give the nextState when getNextState is accessed',
+    () => {
+      const signal1 = new Signal();
+
+      const state1 = signal1.getNextState();
+
+      assert.equal(state1, 'green');
+    });
+
+  it('should update index and return next state when getNextState is accessed',
+    () => {
+      const signal1 = new Signal();
+      const signal2 = new Signal();
+
+      const state1 = signal1.getNextState();
+      const state2 = signal2.getNextState();
+
+      assert.ok(signal1.equals(signal2));
+      assert.equal(state1, state2);
+    });
+
+  it('should update state with next state when updateSignal is accessed',
+    () => {
+      const signal1 = new Signal();
+      const signal2 = new Signal();
+
+      signal1.updateState();
+      signal2.updateState();
+
+      assert.ok(signal1.equals(signal2));
+    });
 });
