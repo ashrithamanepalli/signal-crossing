186281d3571bc86e058f783faa144cc583024ddc
Ashritha M <99526903+ashrithamanepalli@users.noreply.github.com>
2022-06-01 09:50:29 +0530

    Added allStates and index properties in Signal class

--- src/signal.js
+++ src/signal.js
@@ -1,9 +1,14 @@
 class Signal {
   constructor() {
+    this.state = 'red';
+    this.allStates = ['green', 'red'];
+    this.index = 0;
   }
 
   equals(otherSignal) {
-    return otherSignal instanceof Signal;
+    return otherSignal instanceof Signal &&
+      this.state === otherSignal.state &&
+      this.index === otherSignal.index;
   }
 }
 
